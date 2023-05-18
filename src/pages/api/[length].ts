cc010724c1c349fe39e2ff6a4db5320b621693db
jemmec <spogjem@gmail.com>
2023-05-19 01:07:23 +0930

    Added api function for fetching lorem ipsum content

---
+++ src/pages/api/[length].ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { LoremIpsum } from "lorem-ipsum";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    const { length } = req.query;
+
+    const lorem = new LoremIpsum({
+        sentencesPerParagraph: {
+            max: 8,
+            min: 4
+        },
+        wordsPerSentence: {
+            max: 16,
+            min: 4
+        }
+    });
+
+    switch (length) {
+        case 'word': {
+            res.status(200).json({ ipsum: lorem.generateWords(1) })
+            break;
+        }
+        case 'sentence': {
+            res.status(200).json({ ipsum: lorem.generateSentences(1) })
+            break;
+        }
+        case 'paragraph': {
+            res.status(200).json({ ipsum: lorem.generateParagraphs(1) })
+            break;
+        }
+        default: {
+            res.status(404).json({ message: `${length} is not a length, please use 'word', 'sentence', or 'paragraph'.` })
+        }
+    }
+}
